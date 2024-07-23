d25630a4d34bdae5dc1984c372e228c126d7c7a9
SURAJ_KUMAR <surajk38656@gmail.com>
2024-07-23 17:40:29 +0530

    Searching method in react

--- Hooks1/src/Components/Search.jsx
+++ Hooks1/src/Components/Search.jsx
@@ -1,8 +1,51 @@
+import {useState} from "react";
+import  axios from "axios";
+import Table from "react-bootstrap/Table";
 const Search = () =>{
+    const[rollno,setRollno] = useState("");
+    const[mydata,setMydata] = useState([]);
+    const mySearch = () =>{
+        let url = `http://localhost:3000/Student?rollno=${rollno}`
+        axios.get(url).then((res)=>{
+            setMydata(res.data);
+        });
+    }
+    const ans = mydata.map((key)=>{
+        return(
+            <>
+                <tr>
+                    <td>{key.rollno}</td>
+                    <td>{key.name}</td>
+                    <td>{key.city}</td>
+                    <td>{key.fees}</td>
+                </tr>
+            </>
+        )
+    })
     return(
         <div style={{backgroundColor:'',width:'100%',height:'100%',textAlign:'center',color:'black',borderRadius:'10px'}}>
-        <h1>SEARCH PAGE!!</h1>
+        <h1>SEARCH DATA</h1><br/>
+        <input type='text' placeholder='enter rollno...' value={rollno} onChange={(e)=>{
+            setRollno(e.target.value)
+        }}/>
+            <button onClick={mySearch}>click</button><br/>
+            <hr/>
+
+            <Table striped bordered hover id='table'>
+                <thead>
+                <tr>
+                    <th>Rollno</th>
+                    <th>name</th>
+                    <th>city</th>
+                    <th>fees</th>
+                </tr>
+                </thead>
+                <tbody>
+                {ans}
+                </tbody>
+            </Table>
         </div>
     )
 }
+// for search in json we have to run command url/?name="value".
 export default Search;
