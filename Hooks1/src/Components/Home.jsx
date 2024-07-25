3f1a58ddadb0c73b67918bdc034d7739039f6a46
SURAJ_KUMAR <surajk38656@gmail.com>
2024-07-25 13:24:22 +0530

    Hooks1->Insert,search,delete,edit

--- Hooks1/src/Components/Home.jsx
+++ Hooks1/src/Components/Home.jsx
@@ -2,8 +2,8 @@
 const Home = () =>{
     return(
         <>
-        <div style={{backgroundColor:'',width:'100%',height:'100%',textAlign:'center',color:'black',borderRadius:'10px'}}>
-        <img src="https://back.pro.atlasconcordeusa.com/storage/app/media/PAGES/og/OG-Homepage.jpg" alt="" height='100%' width='100%'/>
+        <div style={{backgroundColor:'',width:'100%',height:'100%',textAlign:'center',color:'black',borderRadius:'10px',display:'flex',alignItems:'center',justifyContent:'center'}}>
+        <h1>STUDENT MANAGEMENT SYSTEM</h1>
         </div>
         </>
     )
--- Hooks1/src/Components/Layout.jsx
+++ Hooks1/src/Components/Layout.jsx
@@ -1,16 +1,20 @@
 import { Link, Outlet } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+
 const Layout = () => {
+
     return (
         <>
             <Container>
             <Navbar id="navbar">
-                <Container style={{fontWeight:'600'}}>
-                    <Navbar.Brand as={Link} to="home">Portfolio</Navbar.Brand>
+                <Container style={{fontWeight:'500'}}>
+                    <Navbar.Brand as={Link} to="home" style={{fontSize:'25px'}}>Database</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="home">Home</Nav.Link>
                         <Nav.Link as={Link} to="insert">Insert</Nav.Link>
@@ -18,22 +22,34 @@ const Layout = () => {
                         <Nav.Link as={Link} to="update">Update</Nav.Link>
                         <Nav.Link as={Link} to="display">Display</Nav.Link>
                     </Nav>
+                    <Form className="d-flex">
+                        <Button id='btn' ><img src="https://cdn1.iconfinder.com/data/icons/youtube-23/30/Subtract-4-512.png" height='30' width='30'/></Button> &nbsp;&nbsp;
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
                 </Container>
             </Navbar>
             </Container>
             <br/>
             <Container >
-                <div id='layout' style={{height: '75vh', width: '100%',border:'2px solid skyblue',boxShadow:'0px 0px 5px skyblue',borderRadius:'10px' }}>
+                <div id='layout' style={{height: '75vh', width: '100%',border:'1px solid skyblue',borderRadius:'10px' }}>
                     <Outlet />
                 </div>
-                <hr />
+                {/*<hr />*/}
             </Container>
 
-            <Container style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Container style={{ display: 'flex', alignItems: 'center', justifyContent: 'center',textAlign:'center' }}>
 
                 <Row>
                     <Col ></Col>
-                    <Col xs={12}><p>Everythig is reserved by Copyright ©studentmanagementsystem.com</p></Col>
+                    <Col xs={12}>
+                        <p>STUDENT MANAGEMENT SYSTEM <br/>Everythig is reserved by Copyright ©studentmanagementsystem.com
+                        </p></Col>
                     <Col></Col>
                 </Row>
             </Container>
