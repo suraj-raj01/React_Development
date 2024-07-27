74f6b2e165f1cdf26498b5fdb6dbb920890c6c6f
SURAJ_KUMAR <surajk38656@gmail.com>
2024-07-27 12:44:57 +0530

    StudentManagement System

--- Hooks1/src/Components/Layout.jsx
+++ Hooks1/src/Components/Layout.jsx
@@ -1,53 +1,62 @@
 import { Link, Outlet } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const Layout = () => {
-
     return (
         <>
-            <Navbar id="navbar">
-                <Container style={{fontWeight:'500'}}>
-                    <Navbar.Brand as={Link} to="home" style={{fontSize:'25px'}}>Database</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="insert">Insert</Nav.Link>
-                        <Nav.Link as={Link} to="search">Search</Nav.Link>
-                        <Nav.Link as={Link} to="update">Update</Nav.Link>
-                        <Nav.Link as={Link} to="display">Display</Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Button id='btn'><img src="https://cdn1.iconfinder.com/data/icons/youtube-23/30/Subtract-4-512.png" height='25' width='25'/></Button> &nbsp;&nbsp;&nbsp;
-                        <Form.Control
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Container>
-            </Navbar>
-            <br/>
+            <div>
+                <Navbar expand="lg" id='navbar'>
+                    <Container fluid>
+                        <Navbar.Brand as={Link} to="layout" style={{fontWeight:'bold'}}>Database</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="me-auto my-2 my-lg-0"
+                                style={{ maxHeight: '300px' }}
+                                navbarScroll
+                            >
+                                <Nav.Link as={Link} to="home">Home</Nav.Link>
+                                <Nav.Link as={Link} to="insert">Insert</Nav.Link>
+                                <Nav.Link as={Link} to="search">Search</Nav.Link>
+                                <Nav.Link as={Link} to="update">Update</Nav.Link>
+                                <Nav.Link as={Link} to="display">Display</Nav.Link>
+                            </Nav>
+
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <br/>
                 <div id='layout' style={{height: '75vh', width: '100%',border:'1px solid skyblue',borderRadius:'10px' }}>
                     <Outlet />
                 </div>
                 {/*<hr />*/}
 
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center',textAlign:'center' }}>
+                <div style={{fontWeight:'500', display: 'flex', alignItems: 'center', justifyContent: 'center',textAlign:'center' }}>
 
-                <Row>
-                    <Col ></Col>
-                    <Col xs={12}>
-                        <p>STUDENT MANAGEMENT SYSTEM <br/>Everythig is reserved by Copyright ©studentmanagementsystem.com
-                        </p></Col>
-                    <Col></Col>
-                </Row>
+                    <Row>
+                        <Col ></Col>
+                        <Col xs={12}>
+                            <p>STUDENT MANAGEMENT SYSTEM <br/>Everythig is reserved by Copyright ©studentmanagementsystem.com
+                            </p></Col>
+                        <Col></Col>
+                    </Row>
+                </div>
+
             </div>
         </>
     )
