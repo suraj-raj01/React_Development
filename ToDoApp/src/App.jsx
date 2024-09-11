4f196be89c504d173852f8474cf6c3b74e9c4723
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-11 15:48:36 +0530

    ToDoApp

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,90 +1,152 @@
-import { useSelector,useDispatch } from "react-redux";
-import Table from 'react-bootstrap/Table';
-import { addTask,recDelete,complete,uncomplete,editsavedata } from "./todoSlice";
+import { useSelector, useDispatch } from "react-redux";
+import Table from "react-bootstrap/Table";
+import {
+  addTask,
+  recDelete,
+  complete,
+  uncomplete,
+  editsavedata,
+} from "./todoSlice";
 import { useState } from "react";
 import { Container } from "react-bootstrap";
-const App = () =>{
-  const [mytsk,setMytsk] = useState("");
-  const [editBtn,setEditBtn] = useState(true);
-  const [editId,setEditId] = useState("")
-  const mywork = useSelector((state)=>state.todo.task);
+const App = () => {
+  const [mytsk, setMytsk] = useState("");
+  const [editBtn, setEditBtn] = useState(true);
+  const [editId, setEditId] = useState("");
+  const mywork = useSelector((state) => state.todo.task);
   const dispatch = useDispatch();
   console.log(mywork);
-  const recdelete = (id) =>{
-    dispatch(recDelete(id))
-  }
-  const taskComplete = (id)=>{
-    dispatch(complete(id))
-  }
-  const taskUnComplete = (id)=>{
-    dispatch(uncomplete(id))
-  }
-  const editData = (id,work) =>{
+  const recdelete = (id) => {
+    dispatch(recDelete(id));
+  };
+  const taskComplete = (id) => {
+    dispatch(complete(id));
+  };
+  const taskUnComplete = (id) => {
+    dispatch(uncomplete(id));
+  };
+  const editData = (id, work) => {
     setMytsk(work);
-    setEditBtn(false)
-    setEditId(id)
-  }
-  const editSave = () =>{
-    dispatch(editsavedata({id:editId, work:mytsk}))
+    setEditBtn(false);
+    setEditId(id);
+  };
+  const editSave = () => {
+    dispatch(editsavedata({ id: editId, work: mytsk }));
     setEditBtn(true);
-    setMytsk(mytsk)
-    setEditId(editId)
-  }
+    setMytsk(mytsk);
+    setEditId(editId);
+  };
   let sno = 0;
-  const ans = mywork.map((key)=>{
+  const ans = mywork.map((key) => {
     sno++;
-    return(
+    return (
       <>
-      <tr>
-        <td>{sno}</td>
-        <td>{key.status?(<span style={{textDecoration:"line-through",color:'green',fontWeight:'bold'}}>{key.work}</span>):key.work}</td>
-        <td><button onClick={()=>{recdelete(key.id)}}>Delete</button></td>
-        <td>
-          {key.status?(<button onClick={()=>taskUnComplete(key.id)}>Incomplete</button>):(<button onClick={()=>taskComplete(key.id)}>Completed</button>)}
-        </td>
-        <td><button onClick={()=>{editData(key.id,key.work)}}>Edit Data</button></td>
-      </tr>
+        <tr>
+          <td>{sno}</td>
+          <td>
+            {key.status ? (
+              <span
+                style={{
+                  textDecoration: "line-through",
+                  color: "orangered",
+                  fontWeight: "bold",
+                }}
+              >
+                {key.work}
+              </span>
+            ) : (
+              <span style={{ fontWeight: "bold" }}>{key.work}</span>
+            )}
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                recdelete(key.id);
+              }}
+            >
+              Delete
+            </button>
+          </td>
+          <td>
+            {key.status ? (
+              <button onClick={() => taskUnComplete(key.id)}>Incomplete</button>
+            ) : (
+              <button onClick={() => taskComplete(key.id)}>Completed</button>
+            )}
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                editData(key.id, key.work);
+              }}
+            >
+              Edit Data
+            </button>
+          </td>
+        </tr>
       </>
-    )
-  })
+    );
+  });
 
-  return(
+  return (
     <>
-    <Container>
-    <h1>To Do App</h1>
-    Enter your Task : <input type="text" value={mytsk} onChange={(e)=>{setMytsk(e.target.value)}}/>
-    {editBtn?(
-      <button onClick={()=>{dispatch(addTask({id:Math.random()*100, work:mytsk, status:false}))}}>Add Task</button>
-    ):(
-      <button onClick={editSave}>Save Data</button>
-    )}
-    
-    <br /><br /><br />
+      <Container>
+        <h1 style={{ fontWeight: "bold", color: "green" }}>TodoApp</h1>
+        <br />
+        <center>
+          <input
+            type="text"
+            placeholder="Enter your task"
+            value={mytsk}
+            onChange={(e) => {
+              setMytsk(e.target.value);
+            }}
+          />
+          {editBtn ? (
+            <button
+              onClick={() => {
+                dispatch(
+                  addTask({
+                    id: Math.random() * 100,
+                    work: mytsk,
+                    status: false,
+                  })
+                );
+              }}
+            >
+              Add Task
+            </button>
+          ) : (
+            <button onClick={editSave}>Save Data</button>
+          )}
+        </center>
 
-    <div style={
-      {
-        height:'80vh',
-        width:'100%',
-        overflowY:'scroll'
-      }
-    }>
-    <Table striped bordered hover variant="dark">
-      <thead>
-      <tr>
-        <th>Sno</th>
-        <th>Your Task</th>
-        <th>Update</th>
-        <th>Complete / Incomplete</th>
-        <th>
-          Update
-        </th>
-      </tr>
-      {ans}
-      </thead>
-      </Table>
-    </div>
-    </Container>
+        <br />
+        <br />
+        <br />
+
+        <div
+          style={{
+            height: "70vh",
+            width: "100%",
+            overflowY: "scroll",
+          }}
+        >
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>S No.</th>
+                <th>Your Task</th>
+                <th>Update</th>
+                <th>Complete / Incomplete</th>
+                <th>Edit Data</th>
+              </tr>
+              {ans}
+            </thead>
+          </Table>
+        </div>
+      </Container>
     </>
-  )
-}
+  );
+};
 export default App;
--- ToDoApp/src/main.jsx
+++ ToDoApp/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './store.jsx';
 import { Provider } from 'react-redux';
+import "./style/style.css"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
